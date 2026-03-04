7861225c58e3320ef99a417ae999d1db2ddfd0ac
Jakub Bilski <jakub@blsk.pl>
2026-03-04 09:02:20 +0100

    Implementation of max3 function.

--- src/main.js
+++ src/main.js
@@ -8,3 +8,16 @@ function isOdd(value) {
 
 console.log(isEven(13));
 console.log(isOdd(13));
+
+
+function max3(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+
+console.log(max3(15, 3, 5));
